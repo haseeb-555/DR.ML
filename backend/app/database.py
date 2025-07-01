8f3ccea1948cf7f707498b46681fc04f95719341
obi-tobi <mohammed.20223153@mnnit.ac.in>
2025-07-01 12:22:26 +0530

    cloud db setup

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,14 +2,27 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 
-SQLALCHEMY_DATABSAE_URL  = "postgresql://postgres:Vivekreddy%40123@localhost:5432/DRML"
+# Get path to agents/.env
+env_path = Path(__file__).resolve().parents[1] / ".env"
+# Load local agents/.env
+load_dotenv(dotenv_path=env_path)
+
+
+SQLALCHEMY_DATABSAE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABSAE_URL)
 
-SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base=declarative_base()
+Base = declarative_base()
+
+print("Using DB URL:", os.getenv("DATABASE_URL"))
+
+
 # Dependency
 def get_db():
     db = SessionLocal()
